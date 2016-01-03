38bdb4b80ea4df59ab2347df2f424ef7e6ea5031
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2016-01-03 15:33:43 +0000

    improving result output.

--- tasks/decomment.js
+++ tasks/decomment.js
@@ -1,25 +1,22 @@
 'use strict';
 
 var decomment = require('decomment');
+var color = require('cli-color');
 
 module.exports = function (grunt) {
 
     grunt.registerMultiTask('decomment', 'Removes comments from files.', function () {
 
         var files = this.files.map(function (f) {
-                var result = {
-                    src: f.orig.src.length ? f.orig.src[0] : '',
-                    dest: f.dest
-                }
-                if (grunt.file.exists(result.src)) {
-                    return result;
-                }
-                grunt.log.warn('Source file "' + result.src + '" not found.');
-                return null;
-            })
-            .filter(function (f) {
-                return !!f;
-            });
+            var result = {
+                src: f.orig.src.length ? f.orig.src[0] : '',
+                dest: f.dest
+            };
+            if (grunt.file.exists(result.src)) {
+                return result;
+            }
+            throw new Error("File '" + result.src + "' not found.");
+        });
 
         var type, opt = this.options();
 
@@ -35,16 +32,19 @@ module.exports = function (grunt) {
                 break;
         }
 
+        grunt.log.writeln();
+
         files.forEach(function (f) {
             var code = grunt.file.read(f.src);
             var result;
             try {
                 result = type(code, opt);
             } catch (e) {
-                grunt.log.warn("Failed to decomment file '" + f.src + "'");
+                grunt.log.writeln(f.src + " - " + color.redBright("FAIL"));
                 throw e;
             }
             grunt.file.write(f.dest, result);
+            grunt.log.writeln(f.src + " - " + color.green("OK"));
         });
     });
 };
