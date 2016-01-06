920c87b435afaf7dab9250d69d48462ba0ef4526
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2016-01-06 13:14:24 +0000

    refactoring the code, adding color messages.

--- tasks/decomment.js
+++ tasks/decomment.js
@@ -8,42 +8,42 @@ module.exports = function (grunt) {
 
     grunt.registerMultiTask('decomment', 'Removes comments from files.', function () {
 
-        var type, opt = this.options();
+        var type, name = 'decomment', opt = this.options();
 
         switch (opt.type) {
             case 'text':
                 type = decomment.text;
+                name += '.text';
                 break;
             case 'html':
                 type = decomment.html;
+                name += '.html';
                 break;
             default:
                 type = decomment;
                 break;
         }
 
-
         this.files.forEach(function (byDest) {
             var cwd = byDest.cwd || '';
             var dest = path.join(cwd, byDest.dest);
 
-            byDest.src.forEach(function(f){
-               	var file = path.join(cwd,  f);
-                var outFile = grunt.file.isDir(dest) ? path.join(dest,  f) : dest;
+            byDest.src.forEach(function (f) {
+                var file = path.join(cwd, f);
+                var outFile = grunt.file.isDir(dest) ? path.join(dest, f) : dest;
                 var code = grunt.file.read(file);
                 var result;
                 try {
                     result = type(code, opt);
                 } catch (e) {
-                    grunt.log.writeln(file + " - " + (opt.type ? opt.type + ' type comments' : 'comments') + " - " + color.redBright("FAIL"));
+                    grunt.log.writeln(color.cyan(name + '(') + file + color.cyan(')') + ' - ' + color.redBright("FAIL"));
                     throw e;
                 }
                 grunt.file.write(outFile, result);
                 grunt.log.writeln(outFile + " - " + color.green("OK"));
             })
         });
-        grunt.log.writeln('Decomment - ' + (opt.type ? opt.type + ' type comments' : 'comments') + ' - '  + color.green("OK"));
+        grunt.log.writeln(color.cyan(name + '()') + ' - ' + color.green("OK"));
 
     });
 };
-
